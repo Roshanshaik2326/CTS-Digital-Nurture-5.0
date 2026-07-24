939386e025b16da1b230cd121c6d6b061147cf1a
Roshanshaik2326 <roshansk2326@gmail.com>
2026-07-24 17:30:18 +0530

    feat(module-10): complete HOL 5 Reactive Forms

---
+++ DeepSkilling/Module-10-Angular/student-course-portal/src/app/pages/reactive-enrollment-form/reactive-enrollment-form.ts
@@ -0,0 +1,176 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  FormArray,
+  Validators,
+  FormControl,
+  AbstractControl,
+  ValidationErrors,
+  AsyncValidatorFn
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-enrollment-form',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './reactive-enrollment-form.html',
+  styleUrl: './reactive-enrollment-form.css'
+})
+export class ReactiveEnrollmentFormComponent implements OnInit {
+
+  enrollForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+
+    this.enrollForm = this.fb.group({
+
+      studentName: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3)
+        ]
+      ],
+
+      studentEmail: this.fb.control(
+        '',
+        [
+          Validators.required,
+          Validators.email
+        ],
+        [
+          this.simulateEmailCheck()
+        ]
+      ),
+
+      courseId: [
+        '',
+        [
+          Validators.required,
+          this.noCourseCode
+        ]
+      ],
+
+      preferredSemester: [
+        'Odd',
+        Validators.required
+      ],
+
+      agreeToTerms: [
+        false,
+        Validators.requiredTrue
+      ],
+
+      additionalCourses: this.fb.array([])
+
+    });
+
+  }
+
+  /*
+    value()
+    Returns only enabled controls.
+
+    getRawValue()
+    Returns ALL controls including disabled controls.
+  */
+
+  onSubmit(): void {
+
+    console.log('Form Value');
+
+    console.log(this.enrollForm.value);
+
+    console.log('Raw Value');
+
+    console.log(this.enrollForm.getRawValue());
+
+  }
+
+  noCourseCode(control: AbstractControl): ValidationErrors | null {
+
+    const value = control.value;
+
+    if (value && value.toString().startsWith('XX')) {
+
+      return {
+        noCourseCode: true
+      };
+
+    }
+
+    return null;
+
+  }
+
+  simulateEmailCheck(): AsyncValidatorFn {
+
+    return (control: AbstractControl) => {
+
+      return new Promise<ValidationErrors | null>((resolve) => {
+
+        setTimeout(() => {
+
+          if (
+            control.value &&
+            control.value.includes('test@')
+          ) {
+
+            resolve({
+              emailTaken: true
+            });
+
+          } else {
+
+            resolve(null);
+
+          }
+
+        }, 800);
+
+      });
+
+    };
+
+  }
+
+  get additionalCourses(): FormArray<FormControl> {
+
+  return this.enrollForm.get(
+    'additionalCourses'
+  ) as FormArray<FormControl>;
+
+}
+
+ addCourse(): void {
+
+  this.additionalCourses.push(
+
+    new FormControl<string>(
+      '',
+      {
+        nonNullable: true,
+        validators: [Validators.required]
+      }
+    )
+
+  );
+
+}
+
+  removeCourse(index: number): void {
+
+    this.additionalCourses.removeAt(index);
+
+  }
+
+}
